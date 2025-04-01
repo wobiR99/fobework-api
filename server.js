ee5eb9f709da6049df3a8ed9af4b1c0ef59fb2fa
Obi ifeanyi <obiifeanyi.fi@gmail.com>
2025-04-01 17:12:41 +0100

    "Fix JSON Server issue"

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 10000; // Ensure it runs on allowed ports
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
